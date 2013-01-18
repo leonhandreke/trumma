a8b04b209e954ebe24c67196c14815661cd89e86
Niklas Goerke <niklas@niklasgoerke.de>
2013-01-18 17:25:14 +0100

    implement list file command in ui

--- ui.py
+++ ui.py
@@ -7,37 +7,59 @@ from gevent import monkey
 from peerlist import peerlist
 monkey.patch_sys()
 
+
 def run_ui():
     while True:
         try:
             cmd = raw_input("> ")
 
             # split command
-            scmd = cmd.split();
-
+            if cmd != "":
+                scmd = cmd.split()
             # some alias commands first:
-            if cmd == "list my files": cmd = "list files 127.0.0.1"
-            elif cmd == "listpeers": cmd = "list peers"
-            elif (scmd[0] == "refresh" or scmd[0] == "update") and scmd[1] == "filelist" and len(scmd) == 3: cmd = "get filelist " + scmd[2]
+            if cmd == "list my files":
+                cmd = "list files 127.0.0.1"
+            elif cmd == "listpeers":
+                cmd = "list peers"
+            elif ((scmd[0] == "refresh" or scmd[0] == "update") and
+                scmd[1] == "filelist" and len(scmd) == 3):
+                cmd = "get filelist " + scmd[2]
             # split again, we may have changed cmd
-            scmd = cmd.split();
 
-            if (cmd == "list peers"):
+            if cmd != "":
+                scmd = cmd.split()
+
+            if cmd == "list peers":
                 print(*peerlist, sep="\n")
             elif cmd == "self":
                 print(peerlist.self_peer)
-            elif cmd.startswith("list files ") and len(scmd) == 3:  
-                print(filter(lambda s: s.address==scmd[2], peerlist))
+            elif cmd.startswith("list files ") and len(scmd) >= 3:
+                matchingpeers = []
+                query = ' '.join(scmd[2:])
+                for peer in peerlist:
+                    if peer.alias == query or peer.address == scmd[2]:
+                        matchingpeers.append(peer)
+
+                if len(matchingpeers) > 1:
+                    print("The following peers match your search, please " +
+                            "specify the peer whose files you want to see " +
+                            "by typing \"list peers $ip\"")
+                elif len(matchingpeers) == 0:
+                    print('No peer "' + query + '" found.')
+                else:
+                    for peer in matchingpeers:
+                        print(filter(
+                            lambda s: s.address == peer.address, peerlist))
             elif cmd.startswith("get filelist ") and len(scmd) == 3:
                 print("get filelist " + scmd[2])
             elif cmd.startswith("get file ") and len(scmd) == 4:
                 print("get file " + scmd[2] + " " + scmd[3])
             elif cmd.startswith("find peers"):
                 print("find peers")
-            elif cmd == "exit" or cmd == "quit" or cmd =="q":
+            elif cmd == "exit" or cmd == "quit" or cmd == "q":
                 break
             elif cmd == "help" or cmd == "h":
-                print("there should be some help here…") 
+                print("there should be some help here…")
             else:
                 print("No such command - type help to get a list of commands")
         except EOFError:
