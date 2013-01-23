f639c05b5ababfff7bca32e7c8bad9d79a455bcd
Leon Handreke <leonh@ndreke.de>
2013-01-23 12:06:22 +0100

    Correct separator spelling

--- parser.py
+++ parser.py
@@ -4,16 +4,16 @@ from message import *
 from settings import DEBUG
 
 if DEBUG:
-    DEBUG_message_seperator = u"\\"
-    DEBUG_field_seperator = u"|"
+    DEBUG_message_separator = u"\\"
+    DEBUG_field_separator = u"|"
 
 # ASCII control character 30 (Record Separator, U+001E)
 # is used as a message separator.
-message_seperator = u"\u001e"
+message_separator = u"\u001e"
 
 # ASCII control caracter 31 (Unit Separator, U+001F)
 # is used as a field separator
-field_seperator = u"\u001f"
+field_separator = u"\u001f"
 
 
 """ Message Examples:
@@ -68,7 +68,7 @@ def parse(data):
     return _parseType(data)
 
 
-def build(message, seperator=field_seperator):
+def build(message, separator=field_separator):
     """
     Given a Message object, construct a str Object to be transmitted over the
     network.
@@ -76,15 +76,15 @@ def build(message, seperator=field_seperator):
     if DEBUG:
         print "built: %s" % __str__(message)
 
-    return seperator.join([message.typ, ] + message.fields)
+    return separator.join([message.typ, ] + message.fields)
 
 
 def __str__(message):
-    return DEBUG_field_seperator.join([message.typ, ] + message.fields)
+    return DEBUG_field_separator.join([message.typ, ] + message.fields)
 
 
 def __splitFirstField(text):
-    result = text.split(field_seperator, 1)
+    result = text.split(field_separator, 1)
     if len(result) == 1:
         return result[0], ""
     else:
@@ -92,7 +92,7 @@ def __splitFirstField(text):
 
 
 def __splitFields(text):
-    return text.split(field_seperator)
+    return text.split(field_separator)
 
 
 def _parseType(text):
