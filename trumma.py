1e0bb5ec74bb88375d4a890b458331178b017fde
Niklas Goerke <niklas@niklasgoerke.de>
2013-01-26 15:35:45 +0100

    fixed sending yo packet on receiving hi packet from myself

--- trumma.py
+++ trumma.py
@@ -69,6 +69,7 @@ ipv4_datagram_server.start()
 
 #Greenlet.spawn(run_ui(ipv4_datagram_server, ipv6_datagram_server)).join()
 
+ipv4_datagram_server.send_hi_message_to_multicast_group()
 Greenlet.spawn(run_ui(ipv4_datagram_server)).join()
 
 ipv4_connection_server.stop()
